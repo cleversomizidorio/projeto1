7ecd2acf6bfa0716d7bdbbaf0110adfedf62c752
Cleversomizidorio <cleversonizidoio62@gmail.com>
2024-10-19 20:56:13 -0300

    commit

---
+++ recipes/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+# Create your views here.
+def home(request):
+    return HttpResponse("Home1")
+
+def contato(request):
+    return HttpResponse("contato")
+
+def sobre(request):
+    return HttpResponse("sobre")
